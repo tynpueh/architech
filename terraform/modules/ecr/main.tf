9191786f8d9e93d759bc3ea73d2a3751b0f369e7
tynp <>
2025-11-11 09:19:54 +0700

    feat: enhance ECR repository configuration with image scanning and encryption settings

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,12 @@
 resource "aws_ecr_repository" "app" {
     name = "${var.repository_name}-${var.environment}"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+    scan_on_push = true
+    }
+    encryption_configuration {
+      encryption_type = "AES256"
+    }
 
     tags = {
         Name = "${var.repository_name}-${var.environment}"
