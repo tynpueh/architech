0c457017ef148cbaef95262cecdb15109cc24143
tynp <>
2025-11-16 13:23:28 +0700

    feat: add outputs for ECS cluster and service names in outputs.tf

---
+++ terraform/modules/ecs/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+    description = "The name of the ECS cluster"
+    value       = aws_ecs_cluster.this.name
+  
+}
+
+output "service_name" {
+    description = "The name of the ECS service"
+    value       = aws_ecs_service.app.name
+  
+}
